f823b5f355d404477f538ad49132a66bdff89296
Xoi Hin <xoihin@yahoo.com>
2015-09-17 18:14:08 -0400

    Assignment 46 completed, Tools To Reduce Bugs

--- static-analysis-exercises/BLCViewController.m
+++ static-analysis-exercises/BLCViewController.m
@@ -23,9 +23,10 @@
     NSMutableArray *itemArray = [NSMutableArray array];
     
     int one = 1;
-    int two;
+    int two = 2;
     int three = 3;
     int four = 4;
+    int five =5;
     
     NSString *item1;
     NSString *item2;
@@ -37,19 +38,15 @@
     item2 = [NSString stringWithFormat:@"%d. Bravo", two];
     item3 = [NSString stringWithFormat:@"%d. Charlie", three];
     item4 = [NSString stringWithFormat:@"%d. Delta", four];
+    item5 = [NSString stringWithFormat:@"%d. Emma", five];
     
     [itemArray addObject:item1];
     [itemArray addObject:item2];
     [itemArray addObject:item3];
+    [itemArray addObject:item4];
     [itemArray addObject:item5];
-    
-    NSMutableString *textStrimg;
-    
-    for (NSString *itemString in itemArray) {
-        [textStrimg appendFormat:@"%@\n", itemString];
-    }
-    
-    self.textView.text = textStrimg;
+
+    self.textView.text = [itemArray componentsJoinedByString:@"\n"];
 }
 
 @end
